a538865be8fc4722c39915fb0fdbf193888f64d8
AliNamavar <alinamavar315@gmail.com>
2025-01-09 10:29:16 +0330

    download DRF

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from todo.models import todo
 # Create your views here.
 
 def index_page(request):
-    return render(request, 'home/index.html', {})
+    context = {
+        'todos': todo.objects.order_by('priority').all()
+    }
+    return render(request, 'home/index.html', context)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class todo(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    priority = models.IntegerField(default=1)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = 'todos'
+        
