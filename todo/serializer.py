7bec904fbd343ac8fc3950ca23c01af22ddc11ed
AliNamavar <alinamavar315@gmail.com>
2025-01-12 07:13:55 +0330

    use resframework serializer

---
+++ todo/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import todo 
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = todo
+        fields = ['title', 'content']
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from .models import todo
+from .serializer import TodoSerializer
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET'])
+def all_todos(request: Request):
+    todos = todo.objects.all()
+    todo_serializer = TodoSerializer(todos, many=True)
+    return Response(todo_serializer.data, status.HTTP_200_OK)
